04c8ab3a4766b9e1bcc2b1112b3b3a646e55134f
Kyle Machulis <kyle@nonpolynomial.com>
2013-08-11 13:28:09 -0700

    Cleaning up old code, changing names to be more clear and not conflict with element ids Modulizing FeelGIF so we don't have globals

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 
 $(document).ready(function() {
 
-  var use_old = false;
-
   var PointFrameArray = function(x, y) {
     var points = [];
 
@@ -163,143 +161,148 @@ $(document).ready(function() {
     };
   };
 
-  var sprite;
-  var points = [];
-  var canvas;
-  var ctx;
+  var FeelGIF = (function () {
 
-  function on_point_list_click(e) {
-    document.getElementById("points").removeChild(this);
-  }
+    var sprite;
+    var points = [];
+    var canvas;
+    var ctx;
 
-  function on_canvas_click(e) {
-    var coords = canvas.relMouseCoords(e);
-    if(coords.x > 0 & coords.y > 0 & coords.x < canvas.width & coords.y < canvas.height) {
-      points.push(new PointFrameArray(coords.x, coords.y));
-      draw_circle(coords.x, coords.y);
-      var li = document.createElement("li");
-      li.setAttribute("rel:index", points.length - 1);
-      var text = document.createTextNode(coords.x.toString() + ", " + coords.y.toString());
-      li.appendChild(text);
-      li.addEventListener("click", on_point_list_click);
-      document.getElementById("points").appendChild(li);
+    function on_point_list_click(e) {
+      document.getElementById("pointlist").removeChild(this);
     }
-  }
 
-  function draw_circle(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 4, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  function relMouseCoords(event) {
-    var totalOffsetX=0,totalOffsetY=0,canvasX=0,canvasY=0;
-    var currentElement = this;
-
-    do {
-      totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-      totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-    } while(currentElement = currentElement.offsetParent)
-
-    canvasX = event.pageX - totalOffsetX;
-    canvasY = event.pageY - totalOffsetY;
-
-    return {x:canvasX, y:canvasY};
-  }
-
-  HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
-
-  var drawPoints = function() {
-    var p;
-    var cur_frame = (sprite.get_current_frame() - 1);
-    for(var i = 0; i < points.length; ++i) {
-      p = points[i].get_frame_point(cur_frame);
-      if (p) {
-        draw_circle(p.x, p.y);
+    function on_canvas_click(e) {
+      var coords = canvas.relMouseCoords(e);
+      if(coords.x > 0 & coords.y > 0 & coords.x < canvas.width & coords.y < canvas.height) {
+        points.push(new PointFrameArray(coords.x, coords.y));
+        draw_circle(coords.x, coords.y);
+        var li = document.createElement("li");
+        li.setAttribute("rel:index", points.length - 1);
+        var text = document.createTextNode(coords.x.toString() + ", " + coords.y.toString());
+        li.appendChild(text);
+        li.addEventListener("click", on_point_list_click);
+        document.getElementById("pointlist").appendChild(li);
       }
     }
-  };
-  document.addEventListener('gifmove', drawPoints, false);
+
+    function draw_circle(x, y) {
+      ctx.beginPath();
+      ctx.arc(x, y, 4, 0, Math.PI*2, true);
+      ctx.closePath();
+      ctx.fill();
+    }
+
+    function relMouseCoords(event) {
+      var totalOffsetX=0,totalOffsetY=0,canvasX=0,canvasY=0;
+      var currentElement = this;
+
+      do {
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+      } while(currentElement = currentElement.offsetParent)
+
+      canvasX = event.pageX - totalOffsetX;
+      canvasY = event.pageY - totalOffsetY;
+
+      return {x:canvasX, y:canvasY};
+    }
+
+    HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
+    var drawPoints = function() {
+      var p;
+      var cur_frame = (sprite.get_current_frame() - 1);
+      for(var i = 0; i < points.length; ++i) {
+        p = points[i].get_frame_point(cur_frame);
+        if (p) {
+          draw_circle(p.x, p.y);
+        }
+      }
+    };
+    document.addEventListener('gifmove', drawPoints, false);
 
 
-  var setupGifControls = function() {
-    document.getElementById("stop").style.color = "red";
-    document.getElementById("play").style.color = "black";
-    document.getElementById("pause").style.color = "black";
-    document.getElementById("loopgif").style.color = "black";
-
-    document.getElementById("play").addEventListener("click", function() {
-      document.getElementById("play").style.color = "red";
-      document.getElementById("stop").style.color = "black";
-      document.getElementById("pause").style.color = "black";
-      sprite.play();
-    });
-    document.getElementById("pause").addEventListener("click", function() {
-      document.getElementById("play").style.color = "black";
-      document.getElementById("stop").style.color = "black";
-      document.getElementById("pause").style.color = "red";
-      sprite.pause();
-    });
-    document.getElementById("stop").addEventListener("click", function() {
-      document.getElementById("play").style.color = "black";
+    var setupGifControls = function() {
       document.getElementById("stop").style.color = "red";
+      document.getElementById("play").style.color = "black";
       document.getElementById("pause").style.color = "black";
-      sprite.pause();
-      sprite.move_to(0);
-    });
-    document.getElementById("backward").addEventListener("click", function() {
-      sprite.pause();
-      sprite.move_relative(-1);
-    });
-    document.getElementById("forward").addEventListener("click", function() {
-      sprite.pause();
-      sprite.move_relative(1);
-    });
-    document.getElementById("loopgif").addEventListener("click", function() {
-      if(sprite.get_looping()) {
-        sprite.setloop(false);
-        document.getElementById("loopgif").style.color = "black";
-      } else {
-        sprite.setloop(true);
-        document.getElementById("loopgif").style.color = "red";
-      }
-    });
-    document.getElementById("analyze").addEventListener("click", function() {
-      var o = new OpticalFlowAnalyzer();
-      document.removeEventListener('gifmove', drawPoints, false);
-      o.analyze(sprite, points);
-      document.addEventListener('gifmove', drawPoints, false);
-    });
-    function updateControls(event) {
-      if(!sprite.get_looping()) {
+      document.getElementById("loopgif").style.color = "black";
+
+      document.getElementById("play").addEventListener("click", function() {
+        document.getElementById("play").style.color = "red";
+        document.getElementById("stop").style.color = "black";
+        document.getElementById("pause").style.color = "black";
+        sprite.play();
+      });
+      document.getElementById("pause").addEventListener("click", function() {
+        document.getElementById("play").style.color = "black";
+        document.getElementById("stop").style.color = "black";
+        document.getElementById("pause").style.color = "red";
+        sprite.pause();
+      });
+      document.getElementById("stop").addEventListener("click", function() {
         document.getElementById("play").style.color = "black";
         document.getElementById("stop").style.color = "red";
         document.getElementById("pause").style.color = "black";
+        sprite.pause();
+        sprite.move_to(0);
+      });
+      document.getElementById("backward").addEventListener("click", function() {
+        sprite.pause();
+        sprite.move_relative(-1);
+      });
+      document.getElementById("forward").addEventListener("click", function() {
+        sprite.pause();
+        sprite.move_relative(1);
+      });
+      document.getElementById("loopgif").addEventListener("click", function() {
+        if(sprite.get_looping()) {
+          sprite.setloop(false);
+          document.getElementById("loopgif").style.color = "black";
+        } else {
+          sprite.setloop(true);
+          document.getElementById("loopgif").style.color = "red";
+        }
+      });
+      document.getElementById("analyze").addEventListener("click", function() {
+        var o = new OpticalFlowAnalyzer();
+        document.removeEventListener('gifmove', drawPoints, false);
+        o.analyze(sprite, points);
+        document.addEventListener('gifmove', drawPoints, false);
+      });
+      function updateControls(event) {
+        if(!sprite.get_looping()) {
+          document.getElementById("play").style.color = "black";
+          document.getElementById("stop").style.color = "red";
+          document.getElementById("pause").style.color = "black";
+        }
       }
-    }
-    document.addEventListener('gifloop', updateControls, false);
-  };
+      document.addEventListener('gifloop', updateControls, false);
+    };
 
-  document.getElementById("gifsubmit").addEventListener('click', (function() {
-    var drawdiv = document.getElementById("pregifcanvas");
-    var src;
-    sprite = new SpriteCanvas({ auto_play: false, rubbable: false });
-    sprite.init();
-    var loader = new GifLoader(sprite.get_loader());
-    if(document.getElementById("giffile").value != "") {
-      src = document.getElementById("giffile").value;
-    } else {
-      src = document.getElementById("gifurl").value;
-    }
-    loader.load(src);
-    canvas = sprite.get_canvas();
-    ctx = canvas.getContext("2d");
-    sprite.setloop(false);
-    drawdiv.appendChild(sprite.get_canvas());
-    setupGifControls();
-    canvas.addEventListener('click', on_canvas_click, false);
-    document.getElementById("fileinput").style.display = "none";
-    document.getElementById("preprocessing").style.display = "block";
-  }));
+    document.getElementById("gifsubmit").addEventListener('click', (function() {
+      var drawdiv = document.getElementById("pregifcanvas");
+      var src;
+      sprite = new SpriteCanvas({ auto_play: false, rubbable: false });
+      sprite.init();
+      var loader = new GifLoader(sprite.get_loader());
+      if(document.getElementById("giffile").value != "") {
+        src = document.getElementById("giffile").value;
+      } else {
+        src = document.getElementById("gifurl").value;
+      }
+      loader.load(src);
+      canvas = sprite.get_canvas();
+      ctx = canvas.getContext("2d");
+      sprite.setloop(false);
+      drawdiv.appendChild(sprite.get_canvas());
+      setupGifControls();
+      canvas.addEventListener('click', on_canvas_click, false);
+      document.getElementById("fileinput").style.display = "none";
+      document.getElementById("preprocessing").style.display = "block";
+    }));
+  });
+
+  FeelGIF();
 });
